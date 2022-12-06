3eb5b2a091043c4761b070d1bc2c57d3e8bc86cb
nregsmangattidam <111747229+nregsmangattidam@users.noreply.github.com>
2022-12-06 11:48:03 +0530

    Add files via upload

---
+++ nmms.js
@@ -0,0 +1,5 @@
+function nmmsView() {
+    let n = document.getElementById("wc").value;
+    let m = document.getElementById("msr").value
+    window.location.href  = "https://mnregaweb4.nic.in/netnrega/View_NMMS_atten_date_dtl_rpt.aspx?page=&short_name=KL&state_name=KERALA&state_code=16&district_name=KANNUR&district_code=1602&block_name=Kuthuparamba&block_code=1602006&panchayat_name=Mangattidam&panchayat_code=1602006004&fin_year=2022-2023&source=&work_code=1602006004/WC/"+n+"&msr_no="+m+"&AttendanceDate=0&Digest=WgDbv+R3ggDn6Oo8ZgtzLQ";
+}
