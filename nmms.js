5dc6e7aa66fc93f5c3ddc12d90df309ed5e10c4e
nregsmangattidam <111747229+nregsmangattidam@users.noreply.github.com>
2023-01-21 11:42:59 +0530

    Update nmms.js

--- nmms.js
+++ nmms.js
@@ -4,6 +4,6 @@ function nmmsView() {
     let w = document.getElementById("worktype").value;
     document.getElementById('hidden').style.visibility = 'hidden';
     document.getElementById('wait').innerHTML = 'Please wait...';
-   window.location.href  = "https://mnregaweb4.nic.in/nregaarch/View_NMMS_atten_date_dtl_rpt.aspx?page=&short_name=KL&state_name=KERALA&state_code=16&district_name=KANNUR&district_code=1602&block_name=Kuthuparamba&block_code=1602006&panchayat_name=Mangattidam&panchayat_code=1602006004&fin_year=2022-2023&source=&work_code=1602006004/"+w+"/"+n+"&msr_no="+m+"&AttendanceDate=13/01/2023&Digest=o9KjnECM8nxM+f/Bh+dkUQ";
+   window.location.href  = "https://mnregaweb4.nic.in/nregaarch/View_NMMS_atten_date_dtl_rpt.aspx?page=&short_name=KL&state_name=KERALA&state_code=16&district_name=KANNUR&district_code=1602&block_name=Kuthuparamba&block_code=1602006&panchayat_name=Mangattidam&panchayat_code=1602006004&fin_year=2022-2023&source=&work_code=work_code=1602006004/"+w+"/"+n+"&msr_no="+m+"&AttendanceDate=21/01/2023&Digest=G20gYn3y+WJv6sdqVczWyQ";
     console.log(w);
 }
